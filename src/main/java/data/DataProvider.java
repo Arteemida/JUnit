0777077469cd49a34c932920b54288e8873f277a
Diana Ofitserova <dofitserova@readdle.com>
2022-12-15 00:04:05 -0500

    - added DataProvider (RandomList);

--- src/main/java/data/DataProvider.java
+++ src/main/java/data/DataProvider.java
@@ -5,19 +5,24 @@ import java.util.List;
 import java.util.Random;
 
 public class DataProvider {
-    public static String getRandomString() {
-        List<String> stringList = new ArrayList<>();
+    static List<String> stringList = new ArrayList<>();
+    static List<Integer> integerList = new ArrayList<>();
+
+    static {
         stringList.add("Canada");
         stringList.add("USA");
         stringList.add("Ukraine");
+
+        integerList.add(15);
+        integerList.add(20);
+        integerList.add(33);
+    }
+
+    public static String getRandomString() {
         return stringList.get(new Random().nextInt(stringList.size()));
     }
 
     public static Integer getRandomInt() {
-        List<Integer> integerList = new ArrayList<>();
-        integerList.add(15);
-        integerList.add(20);
-        integerList.add(33);
         return integerList.get(new Random().nextInt(integerList.size()));
     }
 }
